bf4f619c6bb547e38fea0a0a50b3b95dafba3fc6
Philippe Hugo <philippe.hugo@swissonline.ch>
2021-01-05 18:16:58 +0100

    Remove unused news api key

--- app/js/.config.js
+++ app/js/.config.js
@@ -1,5 +1,4 @@
 __CONFIG = {
-    "newsapi_key": "", // https://newsapi.org/
     "openweather_key": "", // https://openweathermap.org/
     "spotify_clientid": "", // https://developer.spotify.com/
     "spotify_clientsecret": "" // https://developer.spotify.com/
